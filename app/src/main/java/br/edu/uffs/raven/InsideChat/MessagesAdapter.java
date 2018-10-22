fbdfb1a6117e5a949c717963cae116cbc076556f
arturspon <artursponchi@gmail.com>
2018-10-22 20:10:08 -0300

    22-10-2018 20:10

--- app/src/main/java/br/edu/uffs/raven/InsideChat/MessagesAdapter.java
+++ app/src/main/java/br/edu/uffs/raven/InsideChat/MessagesAdapter.java
@@ -10,12 +10,14 @@ import android.widget.TextView;
 
 import java.util.List;
 
+import br.edu.uffs.raven.Helpers.ProfileHelper;
 import br.edu.uffs.raven.Models.Message;
 import br.edu.uffs.raven.R;
 
 public class MessagesAdapter extends RecyclerView.Adapter<MessagesAdapter.ViewHolder> {
     private Context context;
     private List<Message> messages;
+    private static final int LAYOUT_NORMAL = 0, LAYOUT_CURRENT_USER = 1;
 
     public MessagesAdapter(Context context, List<Message> messages) {
         this.context = context;
@@ -29,7 +31,13 @@ public class MessagesAdapter extends RecyclerView.Adapter<MessagesAdapter.ViewHo
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.row_message, parent, false);
+        View itemView = null;
+
+        if(viewType == LAYOUT_NORMAL)
+            itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.row_message, parent, false);
+        else if(viewType == LAYOUT_CURRENT_USER)
+            itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.row_message_current_user, parent, false);
+
         return new MessagesAdapter.ViewHolder(itemView);
     }
 
@@ -37,6 +45,18 @@ public class MessagesAdapter extends RecyclerView.Adapter<MessagesAdapter.ViewHo
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
         final Message message = messages.get(position);
         holder.tvMessage.setText(message.getText());
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        Message currentMsg = messages.get(position);
+
+        if(currentMsg.getUserId().equals(ProfileHelper.getUserId()))
+            return LAYOUT_CURRENT_USER;
+        else
+            return LAYOUT_NORMAL;
+
+        //return super.getItemViewType(position);
     }
 
     @Override
